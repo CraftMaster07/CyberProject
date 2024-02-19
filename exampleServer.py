d409323be5d910767994fbeea96fdb65fdc934e7
CraftMaster07 <mogobking@gmail.com>
2024-02-20 00:30:27 +0200

    init server

--- exampleServer.py
+++ exampleServer.py
@@ -1,44 +1,49 @@
-'''import socket
-
-serverSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-serverSocket.bind(("0.0.0.0",12345))
-
-backlog = 10
-serverSocket.listen(backlog)
-
-clientSocket, clientAddress = serverSocket.accept()
-clientSocket.recv(1024)'''
 import socket
+from threading import Thread
 
-HOST = '0.0.0.0'  # localhost
-PORT = 8080         # Port to listen on
-BUFFER_SIZE = 1024  # Buffer size for receiving data
+HOST = '0.0.0.0'
+PORT = 8080
+BUFFER_SIZE = 1024
+BACKLOG = 10
 
-# HTTP response template
 HTTP_RESPONSE = """\
 HTTP/1.1 200 OK
 
-Hello, World!
+RESPONSE
 """
 
-def handle_request(client_socket):
-    request_data = client_socket.recv(BUFFER_SIZE).decode()
+
+def initServer():
+    serverSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    serverSocket.bind((HOST, PORT))
+    print("Server is up and running!")
+
+    return serverSocket
+
+
+def handleRequest(clientSocket):
+    request = clientSocket.recv(BUFFER_SIZE).decode()
     print("Request:")
-    print(request_data)
+    print(request)
 
-    # Send HTTP response
-    client_socket.sendall(HTTP_RESPONSE.encode())
-    client_socket.close()
+    response = HTTP_RESPONSE
+    clientSocket.sendall(response.encode())
+    clientSocket.close()
 
-def run_server():
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server_socket:
-        server_socket.bind((HOST, PORT))
-        server_socket.listen(1)
-        print(f"Server listening on port {PORT}...")
 
-        while True:
-            client_socket, _ = server_socket.accept()
-            handle_request(client_socket)
+def runServer():
+    serverSocket = initServer()
+    serverSocket.listen(BACKLOG)
+    print(f"Server listening on port {PORT}...")
+
+    while True:
+        clientSocket, clientAddress = serverSocket.accept()
+        handleRequest(clientSocket)
+
+
+def main():
+    runServer()
+
 
 if __name__ == "__main__":
-    run_server()
+    main()
