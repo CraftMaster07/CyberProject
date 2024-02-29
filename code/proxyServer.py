3b76c993cbb675b8edd029a711b6e7051014f72b
CraftMaster07 <99757789+CraftMaster07@users.noreply.github.com>
2024-02-29 17:48:13 +0200

    Update proxyServer.py

--- code/proxyServer.py
+++ code/proxyServer.py
@@ -10,19 +10,24 @@ BUFFER_SIZE = 1024
 BACKLOG = 10
 
 
+class Server:
+    def __init__(self, host, port):
+        self.host = host
+        self.port = port
+    
+    def connectToServer(self):
+        newServerSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        newServerSocket.connect((self.host, self.port))
+        print("Connected to a server")
+        return newServerSocket
+
+
 def initProxy():
     proxySocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     proxySocket.bind((HOST, PORT))
     print("Proxy is up and running!")
 
     return proxySocket
-
-
-def connectToServer(host, port):
-    newServerSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    newServerSocket.connect((host, port))
-    print("Connected to a server")
-    return newServerSocket
 
 
 def sendMessage(dSock, request):
@@ -47,7 +52,7 @@ def validateMessage():
     return True
 
 
-def handleClient(proxySocket, serverSockets):
+def handleClient(proxySocket: socket.socket, serverSockets: list[socket.socket])
     clientSocket, clientAddress = proxySocket.accept()
     with clientSocket as clientSock:
         clientAddress = clientAddress
@@ -55,6 +60,16 @@ def handleClient(proxySocket, serverSockets):
         sendMessage(serverSockets[0], req)
         rsp = getResponse(serverSockets[0])
         sendMessage(clientSock, rsp)
+
+
+def initServers(serverList: list[tuple[str, int]]) -> list[tuple[str, int]]:
+    continueAsking = True
+    while continueAsking:
+        serverProps: str = input("Enter server's IP and port (i.e. 127.0.0.1, 12345): ")
+        serverProps: list[str] = serverProps.split(",")
+        serverList.append(serverProps)
+    
+    return serverList
 
 
 
